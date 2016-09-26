a431041b4bba03cfef7127bf07d98af3530f9f2d
Drew Swinburne <Drew@Drew-Swinburnes-Computer.local>
2016-09-26 09:30:56 -0400

    added condition check  added condition check

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,8 +1,27 @@
 console.log("JS file is connected to HTML! Woo!")
 
-alert ("JS file is connected to HTML! Woo!")
 
 var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "king";
 var cardFour = "queen";
+
+if (cardOne === cardThree) {
+	alert ("You've found a match!");}
+	else {
+	alert ("Sorry, try again");}
+
+if (cardOne === cardFour) {
+	alert ("You've found a match!");}
+	else {
+	alert ("Sorry, try again");}
+
+if (cardOne === cardFour) {
+	alert ("You've found a match!");}
+	else {
+	alert ("Sorry, try again");}
+
+if (cardTwo === cardThree) {
+	alert ("You've found a match!");}
+	else {
+	alert ("Sorry, try again");}
